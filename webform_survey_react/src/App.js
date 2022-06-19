8a53a816ecbf1c26fd2ecf30b81c60ebb36cf964
Aryan-Bhatt-pro <aryan.bhatt@iiitb.org>
2022-06-19 22:53:53 +0530

    signin pages

--- webform_survey_react/src/App.js
+++ webform_survey_react/src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+	BrowserRouter as Router,
+	Routes,
+	Route,
+	Link
+} from 'react-router-dom';
+
+import SignInUser from './components/signInAdmin';
+import SignInAdmin from './components/signInUser';
+// import Home from './components/Home';
+import {Button} from 'react-bootstrap'; 
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router><div className="App">
+    <ul className="App-header">
+      <li>
+        <Link to="/signInUser">User</Link>
+      </li>
+      <li>
+        <Link to="/signInAdmin">Admin</Link>
+      </li>
+    </ul>
+    {/* <button className="btn btn-primary btn-lg"></button> */}
+    <Button>This is a button</Button>
+   <Routes>
+         {/* <Route exact path='/' element={< Home />}></Route> */}
+         <Route exact path='/signInUser' element={< SignInUser />}></Route>
+         <Route exact path='/signInAdmin' element={< SignInAdmin />}></Route>
+  </Routes>
+  </div></Router>
   );
 }
 
